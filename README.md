b01f8a16652408fbc018de9009be13077b5cee8f
Taoaoxiang <taoaoxiang@gmail.com>
2022-07-22 00:37:45 -0400

    add a modified method

--- README.md
+++ README.md
@@ -7,6 +7,9 @@ Let's draw sphere in different ways!
 - [x] Recursion #1
     - [X] Python
     - [X] C++
+- [x] Loop #2 Geometry
+    - [X] Python
+    - [ ] C++
 - [ ] Others
 
 ***
@@ -66,3 +69,26 @@ Let's draw sphere in different ways!
 
 ***
 
+***
+
+## Loop #2 Geometry
+
+### Benchmark performance: 
+* Python: 0.088s
+* C++: 
+### Total points: 
+* Python: 125858
+* C++: 
+
+### Method: 
+
+1. Iterate Z-axis [0, r], val: c
+2. Iterate X-axis [r, c], val: a 
+3. Get the y-axis coordinate is ~= b, val: b
+4. Draw a point (a, b, c) in the green area  
+6. Draw a point (c, b, a) in the green area
+7. Iterate 8 octants (+,+,+) to (-,-,-)
+
+### Representation (VMD):
+
+![alt text](images/loop_geometry_model.jpg)
--- sphere.py
+++ sphere.py
@@ -3,6 +3,7 @@
 import sys
 sys.setrecursionlimit(1000000000)
 import time
+import math
 
 
 # ! ref: https://en.wikipedia.org/wiki/Octant_(solid_geometry)
@@ -117,6 +118,91 @@ def recursive_sphere(p, r, resolution, limit_min, limit_max):
     
     return
 
+def loop_sphere_geometry(radius, resolution, debug = False):
+    time_start = time.time()
+
+    if radius <= 0:
+        return []
+
+    if resolution > radius:
+        resolution = radius
+
+    set_range = get_range(radius, resolution)
+    # print("set_range => %s" %str(set_range))
+
+    # limit_sq_max = radius + resolution* resolution
+    # limit_sq_min = radius - resolution* resolution
+    limit_sq_max = radius + resolution
+    limit_sq_min = radius - resolution
+
+    len_range = len(set_range)
+
+    global RECURSION_SET
+    RECURSION_SET = set()
+
+    for (i_z, z) in enumerate(set_range):
+        i_x = len_range - 1
+        while i_x >= i_z:
+            
+            y_list_index = []
+            loop_up = True
+            loop_down = True
+            y_up = len_range
+            y_down = -1
+            diff = radius*radius - set_range[i_x]*set_range[i_x] - set_range[i_z]*set_range[i_z]
+            # print("TEST => i_x: %s, i_z: %s, diff: %f " %(i_x, i_z, diff))
+
+            try:
+                diff_sqrt = math.sqrt(diff)
+            except:
+                diff_sqrt = 0
+
+            index_y = min(range(len(set_range)), key=lambda i: abs(set_range[i]-diff_sqrt))
+            s = sum_sqaure(set_range[i_x], set_range[index_y], set_range[i_z])
+            if s >= limit_sq_min and s <= limit_sq_max:
+                y_list_index.append(index_y)
+            y_up = index_y + 1
+            y_down = index_y -1
+            
+            while (y_up < len_range) and loop_up:
+                s = sum_sqaure(set_range[i_x], set_range[y_up], set_range[i_z])
+                if s >= limit_sq_min and s <= limit_sq_max:
+                    y_list_index.append(y_up)
+                else:
+                    loop_up = False
+                y_up += 1
+
+            while (y_down >= 0) and loop_down:
+                s = sum_sqaure(set_range[i_x], set_range[y_down], set_range[i_z])
+                if s >= limit_sq_min and s <= limit_sq_max:
+                    y_list_index.append(y_down)
+                else:
+                    loop_down = False
+                y_down -= 1
+            
+            for i_y in y_list_index:
+                RECURSION_SET.add( (set_range[i_x], set_range[i_y], set_range[i_z]) )
+                # RECURSION_SET.add( (set_range[i_y], set_range[i_x], set_range[i_z]) )
+                RECURSION_SET.add( (set_range[i_z], set_range[i_y], set_range[i_x]) )
+            
+            i_x -= 1
+
+    tot_points = set()
+    if not debug:
+        for (s_x, s_y, s_z) in OCTANTS:
+            for p in RECURSION_SET:
+                tot_points.add((s_x * p[0], s_y * p[1], s_z * p[2]))
+    else:
+        tot_points = RECURSION_SET
+
+    print(len(tot_points))     
+    time_end = time.time()
+    time_duration = time_end - time_start
+    print("Duration (s) [function: loop_sphere_geometry]: %5.3f" %time_duration)
+    
+    return (tot_points)
+
+
 def write_to_pdb(tot_points, fname = "sphere_test_out.pdb"):
     with open(fname, 'w') as fo:
         fo.write('REMARK this is a sphere\n')
@@ -149,6 +235,11 @@ def benchmark_test():
     # Points: 125858
     tot_points= recursion_sphere(1, 0.01)
     write_to_pdb(tot_points, fname = "benchmark_recursion.pdb")
+
+    # Time(s): 0.088
+    # Points: 125858
+    tot_points = loop_sphere_geometry(1, 0.01)
+    write_to_pdb(tot_points, fname = "benchmark_loop_geometry.pdb")
     print("====Benchmark Finished====\n")
 
 def debug_test():
@@ -156,8 +247,11 @@ def debug_test():
     tot_points = loop_sphere(1, 0.01, True)
     write_to_pdb(tot_points, fname = "debug_loop.pdb")
 
-    tot_points= recursion_sphere(1, 0.01, True)
+    tot_points = recursion_sphere(1, 0.01, True)
     write_to_pdb(tot_points, fname = "debug_recursion.pdb")
+
+    tot_points = loop_sphere_geometry(1, 0.01, True)
+    write_to_pdb(tot_points, fname = "debug_loop_geometry.pdb")
     print("====Debug Finished====\n")
 
 if __name__ == "__main__":
